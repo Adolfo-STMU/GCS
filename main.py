353d65276518336d1a0a907aa833dfede006ea2a
Adolfo-STMU <35376002+Adolfo-STMU@users.noreply.github.com>
2021-03-11 13:06:56 -0600

    Updated Main
    Updated main file to allow the display of the ParametersWidget.py

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication
 import sys
 import MainUi
-import MainLayoutTest5
-import Parameters
-import MissionPlanner
-from DigitalClockFile import DigitalClock
+import MainLayoutTest10
+import ParametersWidget
 from PyQt5.QtCore import QTime, QTimer
 
 
 
-class MainWindowUi(MainLayoutTest5.Ui_MainWindow):
+class MainWindowUi(MainLayoutTest10.Ui_MainWindow):
             def __init__(self):
                 super(MainWindowUi, self).__init__()
                 size = app.desktop().availableGeometry()
@@ -18,18 +16,14 @@ class MainWindowUi(MainLayoutTest5.Ui_MainWindow):
             def setupUi(self, mainWindow):
                 super(MainWindowUi, self).setupUi(mainWindow)
 
-                #self.DigitalClock = DigitalClock(self.centralwidget)
-                #self.DigitalClock.setGeometry(QtCore.QRect(110, 190, 200, 100))
-            #def layoutswitch1 (self,):
 
 
+class ParametersWidgetUi(ParametersWidget.Ui_ParametersWidget):
+    def __init__(self):
+       super(ParametersWidgetUi, self).__init__()
 
-#class MissionPlannerWindowUi(MissionPlanner.Ui_Mission_Planner):
-#    def __init__(self):
-#        super(MissionPlannerWindowUi, self).__init__()
-
-#    def setupUi2(self, mainWindow2):
-#        super(MissionPlannerWindowUi, self).setupUi(mainWindow2)
+    def setupUi2(self, mainWindow2):
+        super(ParametersWidgetUi, self).setupUi(mainWindow2)
 
 
 if __name__ == "__main__":
@@ -40,10 +34,11 @@ if __name__ == "__main__":
     ui.setupUi(mainWindow)
     mainWindow.showMaximized()
 
-#    app2 = QtWidgets.QApplication(sys.argv)
-#    mainWindow2 = QtWidgets.QMainWindow()
-#    ui2 = MissionPlannerWindowUi()
-#    ui2.setupUi2(mainWindow2)
-#    mainWindow2.show()
+
+    app2 = QtWidgets.QDockWidget()
+    mainWindow2 = QtWidgets.QDockWidget()
+    ui2 = ParametersWidgetUi()
+    ui2.setupUi2(mainWindow2)
+    mainWindow2.show()
 
     sys.exit(app.exec_())
