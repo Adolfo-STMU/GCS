c3a7ba88181bad24f8465db68353101932286318
Adolfo-STMU <35376002+Adolfo-STMU@users.noreply.github.com>
2021-03-25 18:55:15 -0500

    Updated Main
    Updated Main to make it more readable

--- main.py
+++ main.py
@@ -1,45 +1,23 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication
 import sys
-import MainUi
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
 import MainLayout
-import ParametersWidget
-
-from PyQt5.QtCore import QTime, QTimer
-
 
 
 class MainWindowUi(MainLayout.Ui_MainWindow):
-            def __init__(self):
-                super(MainWindowUi, self).__init__()
-                size = app.desktop().availableGeometry()
-
-            def setupUi(self, mainWindow):
-                super(MainWindowUi, self).setupUi(mainWindow)
-
-
-
-class ParametersWidgetUi(ParametersWidget.Ui_ParametersWidget):
-    def __init__(self):
-       super(ParametersWidgetUi, self).__init__()
-
-    def setupUi2(self, mainWindow2):
-        super(ParametersWidgetUi, self).setupUi(mainWindow2)
+    # creates window with attributes of Qmainwindow
+    def __init__(self, window):
+        super(MainWindowUi, self).setupUi(window)
 
 
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     mainWindow = QtWidgets.QMainWindow()
-    ui = MainWindowUi()
-    ui.setupUi(mainWindow)
+    ui = MainWindowUi(mainWindow)
+    # for var in dir(ui):
+    #    print(var)
     mainWindow.showMaximized()
-
-
-    #app2 = QtWidgets.QDockWidget()
-    #mainWindow2 = QtWidgets.QDockWidget()
-    #ui2 = ParametersWidgetUi()
-    #ui2.setupUi2(mainWindow2)
-    #mainWindow2.show()
-
+    #for var in dir(mainWindow):
+    #    print(var)
     sys.exit(app.exec_())
